ced9ac741726f4ab346a15a112ba4c170d0a1752
Rocío Fani <rociobfani@hotmail.com>
2022-10-28 20:52:32 -0300

    Agrego PAQUETES

---
+++ UI.Desktop/ReporteCursos.Designer.cs
@@ -0,0 +1,60 @@
+﻿
+namespace UI.Desktop
+{
+    partial class ReporteCursos
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rvCursos = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.SuspendLayout();
+            // 
+            // rvCursos
+            // 
+            this.rvCursos.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.rvCursos.Location = new System.Drawing.Point(0, 0);
+            this.rvCursos.Name = "rvCursos";
+            this.rvCursos.ServerReport.BearerToken = null;
+            this.rvCursos.Size = new System.Drawing.Size(800, 450);
+            this.rvCursos.TabIndex = 0;
+            // 
+            // ReporteCursos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.rvCursos);
+            this.Name = "ReporteCursos";
+            this.Text = "Reporte Cursos";
+            this.Load += new System.EventHandler(this.ReporteCursos_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Microsoft.Reporting.WinForms.ReportViewer rvCursos;
+    }
+}
---
+++ UI.Desktop/ReporteCursos.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Business.Logic;
+using Microsoft.Reporting.WinForms;
+
+namespace UI.Desktop
+{
+    public partial class ReporteCursos : UI.Desktop.ApplicationForm
+    {
+        public ReporteCursos()
+        {
+            InitializeComponent();
+        }
+
+        private void ReporteCursos_Load(object sender, EventArgs e)
+        {
+            CursoLogic cl = new CursoLogic();
+            ReportDataSource rds = new ReportDataSource("DataSet2", cl.GetAll());
+            rvCursos.LocalReport.DataSources.Add(rds);
+            this.rvCursos.RefreshReport();
+        }
+    }
+}
