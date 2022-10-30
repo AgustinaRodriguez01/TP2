7bdb3c250a085cd52044950370553c4a1fc73c4a
AgustinaRodriguez01 <agus4956@gmail.com>
2022-10-30 01:44:17 -0300

    Reportes en web

--- UI.Web/Site.Master.designer.cs
+++ UI.Web/Site.Master.designer.cs
@@ -123,6 +123,24 @@ namespace UI.Web
         protected global::System.Web.UI.WebControls.HyperLink cargarNotasLink;
 
         /// <summary>
+        /// Control reportePlanesLink.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink reportePlanesLink;
+
+        /// <summary>
+        /// Control reporteCursosLink.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink reporteCursosLink;
+
+        /// <summary>
         /// Control bodyContentPlaceHolder.
         /// </summary>
         /// <remarks>
